2c395f94c314580ab5a0936fad761f572e40df1b
Kent Collins <kent.r.collins@gmail.com>
2015-09-16 06:13:34 +0100

    Create Adventurer.java

---
+++ Adventurer.java
@@ -0,0 +1,18 @@
+/**
+* Represents an adventurer in a first person adventure role playing game
+*
+* Provided as an example of an OOP Resource class
+*/
+
+public class Adventurer {
+
+  // Adventurer properties (data)
+  
+  
+  // Constructors 
+  
+  
+  // Adventurer methods (behavior)
+
+
+}
