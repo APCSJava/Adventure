479f1bb949d3615385d4af1e9595dd0cf0363b43
Kent Collins <kent.r.collins@gmail.com>
2015-09-16 06:38:08 +0100

    Create OOPResource.java

---
+++ OOPResource.java
@@ -0,0 +1,32 @@
+/**
+*  Example of an Object Oriented Programming Resource class
+*
+*  In this coding pattern, objects are viewed as a collection of STATE and BEHAVIOR.  
+*
+*  When instantiated (created) each instance will have its initial state set, according to one
+*  or more constructors.  This state is kept hidden from outside classes by designating the data 
+*  fields as "private", thus preventing direct access.  Instead, outside classes are required 
+*  to manipulate each instance only through its publicly exposed methods.  This approach is 
+*  referred to as the principle of DATA ENCAPSULATION.
+*/
+
+public class OOPResource{
+
+  // Properties 
+  // The attributes (data) associated with each instance  (i.e. STATE)
+  // Typically, a listing of private variable declarations
+  // Used to 
+  
+  // Constructors
+  // Instructions for initializing a new instance
+  // Responsible for setting initial STATE
+  // A default, no-argument constructor is inserted by Java iff no constructor explicitly provided
+  
+  // Methods
+  // Specifications for what each instance "can do"  (i.e. BEHAVIOR)
+  // Typically, a listing of public accessors and mutators (getters and setters)
+  // Used to interact with an instance
+
+
+
+}
